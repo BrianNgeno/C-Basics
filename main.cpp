97364efaa45c0ac9493db010fbb07eb8c7ea76e7
brian <bkn.ngeno@gmail.com>
2025-08-06 02:05:00 +0300

    updated comments to be more specific

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ using namespace std;
 // main function begins here
 int main(){
     // print output hello world function
+    // cout standard output string 
+    // << insertion operator
     cout << "Hello World\n";
-    // return statement 
+    // return statement exit code 0 return success
     return 0;
 }
