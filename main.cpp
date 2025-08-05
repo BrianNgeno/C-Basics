6ed436413c73913d42ff491fab2bfd69f29b12f6
brian <bkn.ngeno@gmail.com>
2025-08-06 02:02:45 +0300

    added comments

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
+// pre-processor directive iostream library
 #include <iostream>
 
 using namespace std;
-
+// main function begins here
 int main(){
+    // print output hello world function
     cout << "Hello World\n";
+    // return statement 
     return 0;
 }
